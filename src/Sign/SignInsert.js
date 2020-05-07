cfe9dbb7b412e47639b0ed1013277064872c9217
juhee <qkr30510@naver.com>
2020-05-07 15:59:47 +0900

    Update signInsert

--- src/Sign/SignInsert.js
+++ src/Sign/SignInsert.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import styled, { css } from 'styled-components';
 
 const Box = styled.div`
@@ -25,92 +25,139 @@ form{
 `;
 
 const DivWrap = styled.div`
-  
   width: 100%;
   margin-bottom: 1rem;
-  border-bottom:1px solid #666;
- 
-  &:last-child{
-  border-bottom:none;
-}
-  
+  border-bottom: 1px solid #666;
+
+  &:last-child {
+    border-bottom: none;
+    display: flex;
+    justify-content: center;
+
+    button {
+      padding: 0.5rem;
+      font-size: 1.1rem;
+      font-weight: bold;
+      background: #ddd;
+      color: #777;
+      &:last-child {
+        background: #777;
+        color: #eee;
+        margin-left: 1rem;
+      }
+    }
+  }
+
   label {
-    display: block;    
+    display: block;
     width: 100%;
-    margin-bottom:0.3rem;
-    font-weight:bold;
-   
+    margin-bottom: 0.3rem;
+    font-weight: bold;
   }
   input[type='text'] {
-      padding: 5px;
-      background: none;
-      border:none;
-      outline: none;
-      width: 70%;
-      text-align: left;
-    }
-    
-    
-    textarea {
-      width: 100%;
-    }
+    padding: 5px;
+    background: none;
+    border: none;
+    outline: none;
+    width: 70%;
+    text-align: left;
+  }
 
+  textarea {
+    width: 100%;
+  }
 `;
 
 const CheckBoxWrap = styled.div`
-display:flex;
-justify-content:start;
-width:auto;
+  display: flex;
+  justify-content: start;
+  width: auto;
 
-label{
-  font-weight:normal;
-
-}
-`
+  label {
+    font-weight: normal;
+  }
+`;
 
 const Button = styled.button`
-        width: calc(100% - 74%);
-      background:#f2f2f2;
-      border-radius:25px;
-      border:none;
-      padding:0.2rem;
-      margin-bottom:0.5rem;
-      cursor:pointer;
-`
+  width: calc(100% - 74%);
+  background: #f2f2f2;
+  border-radius: 25px;
+  border: none;
+  padding: 0.2rem;
+  margin-bottom: 0.5rem;
+  cursor: pointer;
+`;
 
+const SignInsert = ({ introduce, onChange}) => {
+  const [idValue, setIdValue] = useState('');
+  const [pwValue, setPwValue] = useState('');
+  const [sePwValue, seSetPwValue] = useState('');
 
-
- 
-
-
-const SignInsert = ({ writeId, password, checkpw, introduce, onChange }) => {
-  
   const year = () => {
     const y = [];
-    for (let i=1920; i<2020; i++){           
-      y.push(<option>{i}</option>)    
-     }     
-    return y    
-  }
+    for (let i = 1920; i < 2020; i++) {
+      y.push(<option>{i}</option>);
+    }
+    return y;
+  };
 
   const month = () => {
     const m = [];
-    for(let i=1; i<13; i++){
-      m.push(<option>{i}</option>)
-      
+    for (let i = 1; i < 13; i++) {
+      m.push(<option>{i}</option>);
     }
-    return m
-  }
+    return m;
+  };
 
   const day = () => {
     const d = [];
-    for(let i=1; i<32; i++){
-      d.push(<option>{i}</option>)
-           
+    for (let i = 1; i < 32; i++) {
+      d.push(<option>{i}</option>);
     }
-    return d
+    return d;
+  };
+
+// 아이디 유효성 체크 
+  const regTypeId = /^[a-zA-Z0-9]*$/;
+  if (!regTypeId.test(idValue)){
+    //console.log("아이디엔 영문과 숫자만 가능합니다.",idValue)
   }
 
+  const checkNum = idValue.search(/[0-9]/g);
+  const checkEng = idValue.search(/[a-z]/ig);
+  if(checkNum < 0 || checkEng < 0){
+   // console.log('아이디는 영문과 숫자가 혼용되어야합니다.',idValue)
+  }
+  
+
+  const checkid = useCallback((e) => {
+    setIdValue(e.target.value);  
+      // 정규표현식으로 체크 먼저하고 setValue로 넣기!!
+
+  }, []);
+
+// 비밀번호 유효성 체크 
+  
+  const checkPwNum = pwValue.search(/[0-9]/g);
+  
+  const checkPwEng = pwValue.search(/[a-z]/ig);
+  const checkPwSpe = pwValue.search(/[~!@#$%^&*()_+|<>?:{}]/gi);
+  const checkpwKor = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g;
+  console.log("z",checkPwNum)
+  //const pwCheckRule = /^(?=.*[a-zA-Z])(?=.*[!@#$%])(?=.*[0-9])$/ ;
+  
+
+  if(checkPwNum < 0 || checkPwEng < 0 || checkPwSpe < 0){
+    console.log('비밀번호는 영문과 숫자, 특수문자가 혼용되어야합니다.',pwValue)
+    if(checkpwKor.test(pwValue)){
+      console.log('비밀번호는 한글이 들어갈수 없습니다',pwValue)
+    }
+  }
+  
+  const checkPw = useCallback ((e) => {
+    setPwValue(e.target.value)
+  },[])
+  
   return (
     <Box>
       <h3>회원가입</h3>
@@ -120,10 +167,11 @@ const SignInsert = ({ writeId, password, checkpw, introduce, onChange }) => {
           <input
             type="text"
             name="writeId"
-            value={writeId}
-            onChange={onChange}
-            placeholder="10자리 이상 영문+숫자+특수문자가 결합"
-          />
+            value={idValue}
+            onChange={checkid}
+            placeholder="영문과 숫자를 결합해주세요."
+            id="userid"                                    
+          />           
           <Button>중복체크</Button>
         </DivWrap>
         <DivWrap>
@@ -131,10 +179,9 @@ const SignInsert = ({ writeId, password, checkpw, introduce, onChange }) => {
           <input
             type="text"
             name="password"
-            value={password}
-            onChange={onChange}
-            placeholder="12자리 이상 영문+숫자+특수문자2개이상
-            "
+            value={pwValue}
+            onChange={checkPw}
+            placeholder="6자리 이상 영문+숫자+특수문자"
           />
         </DivWrap>
         <DivWrap>
@@ -142,13 +189,13 @@ const SignInsert = ({ writeId, password, checkpw, introduce, onChange }) => {
           <input
             type="text"
             name="checkpw"
-            value={checkpw}
-            onChange={onChange}
+            value={sePwValue}
+            onChange={checkPw}
           />
         </DivWrap>
         <DivWrap>
           <label>성별</label>
-          <input type="radio" name="gender" value="man"/>
+          <input type="radio" name="gender" value="man" />
           남자
           <input type="radio" name="gender" value="woman" />
           여자
@@ -159,7 +206,7 @@ const SignInsert = ({ writeId, password, checkpw, introduce, onChange }) => {
             {year()}
           </select>
           <select name="month" id="month">
-          {month()}
+            {month()}
           </select>
           <select name="day" id="day">
             {day()}
@@ -168,18 +215,18 @@ const SignInsert = ({ writeId, password, checkpw, introduce, onChange }) => {
         <DivWrap>
           <label>취미</label>
           <CheckBoxWrap>
-          <label>
-            <input type="checkbox" name="exercise" />
-            운동
-          </label>
-          <label>
-            <input type="checkbox" name="game" />
-            게임
-          </label>
-          <label>
-            <input type="checkbox" name="movie" />
-            영화보기
-          </label>
+            <label>
+              <input type="checkbox" name="exercise" />
+              운동
+            </label>
+            <label>
+              <input type="checkbox" name="game" />
+              게임
+            </label>
+            <label>
+              <input type="checkbox" name="movie" />
+              영화보기
+            </label>
           </CheckBoxWrap>
         </DivWrap>
         <DivWrap>
