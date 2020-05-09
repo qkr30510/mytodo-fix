b7fb65f130c8ed96e2d8580799cdcbdb13783c62
juhee <qkr30510@naver.com>
2020-05-09 17:37:13 +0900

    add Login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React  from 'react';
 import Todo from './Todo/Todo';
 import Sign from './Sign/Sign';
+import Login from './Login/Login'
 
 const App = () => {
 
   return (
     <>
-      <Sign/>
+      <Login/>
     </>
   )
 }
